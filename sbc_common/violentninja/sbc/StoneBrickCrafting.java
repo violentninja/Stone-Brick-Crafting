4c63e44a2830ebd13d7a803436099097408c3492
violentninja <d.fern@hotmail.co.uk>
2013-08-20 18:03:18 +0100

    Project beginning!
    The first part of the mod's creation!

---
+++ sbc_common/violentninja/sbc/StoneBrickCrafting.java
@@ -0,0 +1,10 @@
+package violentninja.sbc;
+
+import violentninja.sbc.lib.Reference;
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
+
+public class StoneBrickCrafting {
+
+}
---
+++ sbc_common/violentninja/sbc/lib/Reference.java
@@ -0,0 +1,21 @@
+package violentninja.sbc.lib;
+
+	public class Reference {
+
+	    /**
+	     * The mod id
+	     */
+	    public static final String MOD_ID = "StoneBrickCrafting";
+	    
+	    /**
+	     * The mod name
+	     */
+	    public static final String MOD_NAME = "Stone Brick Crafting";
+	    
+	    /**
+	     * The version of the mod
+	     */
+	    public static final String VERSION = "0.0.1";
+	}
+
+}
